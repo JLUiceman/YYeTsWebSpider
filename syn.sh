3cd672e0d40160eed00f5fc22c0f193de3182ff2
Caiyb <138885146@qq.com>
2017-02-28 15:08:20 +0800

    Update syn.sh

--- syn.sh
+++ syn.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "$(date +"%F-%H%M%S") + <br />" >> README.md
-echo 'success update' >> mylog.log
 
 git add .
-git commit -m "同步于 $(date +"%F-%H%M%S")"
+git commit -m "同步于 $(date +"%F-%H-%-M%-S")"
 git push
